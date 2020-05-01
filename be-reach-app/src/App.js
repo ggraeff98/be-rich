91efb5dc1ea7fdb20b3e3fcf4d60ceaeb41f089a
Gustavo <gustavograeff1998@gmail.com>
2020-05-01 15:43:31 -0300

    add: prevent double click zoom on mobile

--- be-reach-app/src/App.js
+++ be-reach-app/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PageHeader from './components/PageHeader/PageHeader';
 import LateralMenu from './components/LateralMenu/LateralMenu';
+import styled from 'styled-components';
+
+const StyleedDivMain = styled.div`
+  touch-action: manipulation;
+`;
 
 function App() {
   return (
-    <div>
+    <StyleedDivMain>
       <PageHeader></PageHeader>
       <LateralMenu></LateralMenu>
-    </div>
+    </StyleedDivMain>
   );
 }
 
