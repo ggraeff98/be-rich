89e356d45e42819302ed375669b2e407a38d0e47
Gustavo <gustavograeff1998@gmail.com>
2020-05-01 12:37:04 -0300

    ass: style color constants

---
+++ be-reach-app/src/constants/styleConstants.js
@@ -0,0 +1,7 @@
+export const LIGHT_GOLDEN = '#f2b233';
+export const NORMAL_GOLDEN = '#dd981d';
+
+export const NORMAL_WHITE = '#ffffff';
+
+export const NORMAL_GREEN = '#208c61';
+export const LIGHT_GREEN = '#2caf76';
