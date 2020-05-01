2ef7ee30b7b1200016f3b0b15c37653cc47a01f2
Gustavo <gustavograeff1998@gmail.com>
2020-05-01 12:40:52 -0300

    add: header menu items

---
+++ be-reach-app/src/components/PageHeader/HeaderItems/HeaderItems.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NORMAL_WHITE } from '../../../constants/styleConstants';
+
+const StyledUl = styled.ul`
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  height: 100%;
+`;
+
+const StyleedLi = styled.li`
+  font-family: 'Ubuntu';
+  color: ${NORMAL_WHITE};
+  padding: 0 10px;
+  font-size: 20px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    background: rgba(149, 116, 136, 0.2);
+    font-weight: bold;
+    border-bottom: 5px solid rgb(113, 145, 76);
+  }
+
+  div {
+    display: flex;
+    align-items: flex-end;
+    img {
+      width: 30px;
+      height: 35px;
+      padding-right: 5px;
+    }
+  }
+`;
+
+const headerItem = (props) => {
+  const headerItems = props.menuOptions.map((option) => {
+    return (
+      <StyleedLi>
+        <div>
+          <img src={option.imagePath} alt={option.alt}></img>
+          {option.label}
+        </div>
+      </StyleedLi>
+    );
+  });
+
+  return <StyledUl>{headerItems}</StyledUl>;
+};
+
+export default headerItem;
