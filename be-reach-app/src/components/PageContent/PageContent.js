186f9e3a8493ba46b64a14fa2da44c4489b150c6
Gustavo <gustavograeff1998@gmail.com>
2020-05-07 07:48:04 -0300

    add: create page content

---
+++ be-reach-app/src/components/PageContent/PageContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledPageContentContainer = styled.div``;
+
+const pageContent = () => {
+  return (
+    <StyledPageContentContainer>
+      <div>aaaaaaaaaaaaa</div>
+    </StyledPageContentContainer>
+  );
+};
+
+export default pageContent;
