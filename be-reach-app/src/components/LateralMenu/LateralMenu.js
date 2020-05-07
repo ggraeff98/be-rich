1e388fd92c955620f9bdfe08f4f1c444c2fc67f7
Gustavo <gustavograeff1998@gmail.com>
2020-05-07 07:47:31 -0300

    fix: lateral menu now has just needed components

--- be-reach-app/src/components/LateralMenu/LateralMenu.js
+++ be-reach-app/src/components/LateralMenu/LateralMenu.js
@@ -1,15 +1,8 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
-import hamburguerIcon from '../../assets/svgs/hamburguer-menu.svg';
 import { LateralMenuContext } from '../../contexts/lateral-menu-context';
 
 const StyledDivContainer = styled.div``;
-
-const StyledMenuOpenIconContainer = styled.div`
-  background: ${(props) => (!props.isOpen ? 'trasparent' : 'rgba(0, 0, 0, 0.3);')};
-  height: ${(props) => (props.isOpen ? '100vh' : 'unset')};
-  width: ${(props) => (!props.isOpen ? '0px' : '100%')};
-`;
 
 const StyledDivListItems = styled.nav`
   position: absolute;
@@ -21,13 +14,6 @@ const StyledDivListItems = styled.nav`
   top: 0;
 `;
 
-const StyledToggleMenuIcons = styled.img`
-  padding: 5px 10px;
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
 StyledDivListItems.defaultProps = {
   isOpen: false
 };
@@ -35,26 +21,8 @@ StyledDivListItems.defaultProps = {
 const LateralMenu = (props) => {
   const lateralMenuContext = useContext(LateralMenuContext);
 
-  const onChangeMenuLateralState = () => {
-    lateralMenuContext.onToggleLateralMenu();
-  };
-
-  const openLateralMenuIcon = (
-    <StyledToggleMenuIcons
-      src={hamburguerIcon}
-      alt="open-menu"
-      onClick={() => onChangeMenuLateralState()}
-    ></StyledToggleMenuIcons>
-  );
-
   return (
     <StyledDivContainer>
-      <StyledMenuOpenIconContainer
-        onClick={onChangeMenuLateralState}
-        isOpen={lateralMenuContext.lateralMenuState}
-      >
-        {openLateralMenuIcon}
-      </StyledMenuOpenIconContainer>
       <StyledDivListItems isOpen={lateralMenuContext.lateralMenuState}>
         {props.children}
       </StyledDivListItems>
